e0e372640efc2c2542e35315857f3f972be20520
Ungureanu Virgil <uvirgil1901@gmail.com>
2025-03-08 22:14:57 +0200

    Modified list product and delete lines for print

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ from product_manager import ProductManager
 
 product_manager = ProductManager()
 
-product1 = Product("Phone", 899.99, 56)
-product2 = Product("Charger", 9.99, 242)
-product3 = Product("Wireless Charger", 19.99, 123)
-product4 = Product("Phone Case", 5.99, 432)
+product1 = Product("Laptop", 899.99, 56)
+product2 = Product("Charger", 9.99, 212)
+product3 = Product("Wireless Charger", 22.99, 123)
+product4 = Product("Phone Case", 5.99, 398)
 
 product_manager.add_product(product1)
 product_manager.add_product(product2)
 product_manager.add_product(product3)
 product_manager.add_product(product4)
 
-print("Products in the store:")
-product_manager.display_products()
 
-print(f"Total value of products in the store: {product_manager.total_products_value()}£")
